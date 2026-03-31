6d16bccb751c86197a642704df1939a13942e305
zyzyzy29 <b2029zdcabalos@pshs.edu.ph>
2026-03-31 22:44:45 +0800

    3gpair

--- public/3rd Graded/script.js
+++ public/3rd Graded/script.js
@@ -1,28 +1,29 @@
-function plotPoint() {
-    console.log(x0, y0, x, y) // checks the arguments passed to this function
-    in1.innerHTML = x0.value + " " + y0.value + " " + x.value + " " + y.value;
+function plotPoint(x0_val, y0_val, x_val, y_val) {
+    document.getElementById('in1').innerHTML = x0_val + " " + y0_val + " " + x_val + " " + y_val;
+
     var point = document.createElement('div');
     point.className = 'point';
 
-    /* control where to place the div on the screen using left and bottom of position:absolute */
-    point.position = 'absolute';
-    point.style.left = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
-    point.style.bottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
+    point.style.left = (x_val - x0_val + 200 - 5) + 'px'; 
+    point.style.bottom = (y_val - y0_val + 200 - 5) + 'px'; 
+  
     document.getElementById('coordinatePlane').appendChild(point);
 
-    if (x.value > x0.value && y.value > y0.value){
-      out1.innerHTML = "NE"
+    var out1 = document.getElementById('out1');
+    
+    if (x_val > x0_val && y_val > y0_val) {
+      out1.innerHTML = "NE";
     }
-    else if (x.value < x0.value && y.value < y0.value){
-      out1.innerHTML = "SO"
+    else if (x_val < x0_val && y_val < y0_val) {
+      out1.innerHTML = "SO";
     }
-    else if (x.value > x0.value && y.value < y0.value){
-      out1.innerHTML = "SE"
+    else if (x_val > x0_val && y_val < y0_val) {
+      out1.innerHTML = "SE";
     }
-    else if (x.value < x0.value && y.value > y0.value){
-      out1.innerHTML = "NO"
+    else if (x_val < x0_val && y_val > y0_val) {
+      out1.innerHTML = "NO";
     }
-    else{
-      out1.innerHTML = "divisa"
+    else {
+      out1.innerHTML = "divisa";
     }
 }
